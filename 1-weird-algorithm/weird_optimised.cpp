4be24d8485108c7f03c8561ecd3c20f090fc3744
Sahil A <i@sahil.im>
2026-08-17 10:03:53 +0530

    1. Weird Algorithm

---
+++ 1-weird-algorithm/weird_optimised.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n; // long long to handle large values
+    cin >> n;
+
+    while (n != 1) {
+        cout << n << ' '; // cout the value before right shift
+        if ((n & 1) == 0) // check if n is even
+            n >>= 1; // bitwise right shift means divide by 2
+        else {
+            n = (3 * n + 1);
+            cout << n << ' '; // cout the value before right shift
+            n >>= 1; // we know n is even after this so we can safely right shift to optimise
+        }
+    }
+    cout << n << '\n'; // cout the final value
+
+    return 0;
