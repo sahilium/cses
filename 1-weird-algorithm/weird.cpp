aedafc18641e3ff25b5b5ff2227e8f6dca005200
Sahil A <i@sahil.im>
2026-08-18 10:19:20 +0530

    new knowledge!

--- 1-weird-algorithm/weird.cpp
+++ 1-weird-algorithm/weird.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long n;
+    ll n;
     cin >> n;
 
     while (n != 1) {
--- 1-weird-algorithm/weird_optimised.cpp
+++ 1-weird-algorithm/weird_optimised.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long n; // long long to handle large values
+    ll n; // long long to handle large values
     cin >> n;
 
     while (n != 1) {
